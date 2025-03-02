7fb05823a05701555e6b84044d9ed9d38940cf98
Riad2020Math <mdkhalilurrahmanriad00@gmail.com>
2025-03-02 19:09:54 +0600

    first commit

---
+++ link.js
@@ -0,0 +1,10 @@
+document.getElementById("blog-page-btn").addEventListener("click",function (){
+    window.location.href="./blogs.html";
+})
+document.getElementById("back-to-desk").addEventListener("click",function (){
+    window.location.href="./index.html";
+})
+document.getElementById("complete01").addEventListener("click",function (event){
+    event.preventDefault();
+    
+})
